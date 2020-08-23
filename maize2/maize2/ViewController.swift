0be907d2b942434a59a814a1e39e5ec089cf4eb5
Gloryb1234 <68911564+Gloryb1234@users.noreply.github.com>
2020-08-22 20:58:11 -0400

    Add files via upload

---
+++ maize2/maize2/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  maize2
+//
+//  Created by Glorybelle Barrios on 8/22/20.
+//  Copyright © 2020 Glorybelle Barrios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func button1(_ sender: Any) {
+    }
+    
+    
+    @IBAction func button2(_ sender: Any) {
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+    }
+    
+    @IBAction func step1(_ sender: Any) {
+        print(" First and foremost, you must collect seeds- either from the farmer's market or your grocery store.")
+    }
+    
+    
+    @IBAction func step2(_ sender: Any) {
+      print("Upon retrieving the seeds, place them into a glass container. If a glass container is not availbale, enclose them in a paper envelope." )
+        
+    }
+    
+    
+    
+    
+    @IBAction func step3(_ sender: Any) {
+        
+         print("Store the containers in an area where direct sunlight is not present.If you live in a moderately warm area, place the seeds into the refrigerator.")
+    }
+    
+    
+    @IBAction func step4(_ sender: Any) {
+        
+        print("After 12 months of collecting and preserving seeds, plant the seeds into their respective forms of soil (can be found in google)." )
+    }
+
+    @IBAction func type1(_ sender: Any) {
+    }
+    
+    @IBAction func type2(_ sender: Any) {
+    }
+    
+
+    @IBAction func type3(_ sender: Any) {
+    }
+     
+    
+    
+    @IBOutlet weak var textView1: UITextView!
+    
+    @IBOutlet weak var textView2: UITextView!
+    @IBOutlet weak var textView3: UITextView!
+    
+    @IBAction func submit(_ sender: UIButton) {
+        
+    }
+
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
